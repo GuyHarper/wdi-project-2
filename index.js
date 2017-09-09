5087f2f06130104e15aa2a35019ed54611a7a816
= <guy.harper@gmail.com>
2017-09-09 19:44:20 +0100

    Setting up body-parser

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const expressLayouts  = require('express-ejs-layouts');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
+const bodyParser = require('body-parser');
 
 const { port, dbURI } = require('./config/environment');
 const routes = require('./config/routes');
@@ -18,6 +19,7 @@ app.set('views', `${__dirname}/views`);
 app.use(expressLayouts);
 app.use(express.static(`${__dirname}/public`));
 app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(methodOverride((req => {
   if(req.body && typeof req.body === 'object' && '_method' in req.body) {
     const method = req.body._method;
