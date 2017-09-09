4eaaea448bf1f3441bef8bf28d0dc151e323ef21
= <guy.harper@gmail.com>
2017-09-09 17:56:46 +0100

    Creating list and user models

--- models/list.js
+++ models/list.js
@@ -1,1 +1,27 @@
 const mongoose = require('mongoose');
+
+const commentSchema = new mongoose.Schema({
+  text: String,
+  author: { type: mongoose.Schema.ObjectId, ref: 'User' }
+}, {
+  timestamps: true
+});
+
+const entrySchema = new mongoose.Schema({
+  text: String,
+  author: { type: mongoose.Schema.ObjectId, ref: 'User' },
+  active: Boolean,
+  comments: [ commentSchema ]
+}, {
+  timestamps: true
+});
+
+const listSchema = new mongoose.Schema({
+  name: String,
+  entries: [ entrySchema ],
+  contributors: [{ type: mongoose.Schema.ObjectId, ref: 'User' }]
+}, {
+  timestamps: true
+});
+
+module.exports = mongoose.model( 'List', listSchema );
