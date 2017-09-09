c1a0c2a1009109bc84a160df1cef801b5991c2c9
= <guy.harper@gmail.com>
2017-09-09 18:42:08 +0100

    Populating seeds file

--- db/seeds.js
+++ db/seeds.js
@@ -1,6 +1,26 @@
 const mongoose = require('mongoose');
 const List = require('../models/list');
-const dbURI = require('./config/environment');
+const { dbURI } = require('../config/environment');
 
 mongoose.connect(dbURI, { useMongoClient: true });
+
 mongoose.Promise = require('bluebird');
+
+List.collection.drop();
+
+List.create([{
+  name: 'Groceries',
+  entries: [{
+    text: 'Bread',
+    active: true
+  },{
+    text: 'Milk',
+    active: true
+  },{
+    text: 'Biscuits',
+    active: false
+  }]
+}])
+  .then(lists => console.log(`${lists.length} lists created!`))
+  .catch((err) => console.log(err))
+  .finally(() => mongoose.connection.close());
