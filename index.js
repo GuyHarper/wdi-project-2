91fb98f3255e436d443020a19806d00cd1eca817
= <guy.harper@gmail.com>
2017-09-09 17:35:01 +0100

    Setting up morgan

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const express = require('express');
 const app = express();
 const { port } = require('./config/environment');
 const expressLayouts  = require('express-ejs-layouts');
+const morgan = require('morgan');
 
 app.set('view engine', 'ejs');
 app.set('views', `${__dirname}/views`);
 
 app.use(expressLayouts);
 app.use(express.static(`${__dirname}/public`));
+app.use(morgan('dev'));
 
 app.get('/', (req, res) => res.render('home'));
 
