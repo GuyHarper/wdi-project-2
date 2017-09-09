77aa06d13cb70c04757ddb1a358b618a9850488e
= <guy.harper@gmail.com>
2017-09-09 19:42:55 +0100

    Setting up method-override

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const app = express();
-const { port, dbURI } = require('./config/environment');
+
 const expressLayouts  = require('express-ejs-layouts');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
+const methodOverride = require('method-override');
+
+const { port, dbURI } = require('./config/environment');
 const routes = require('./config/routes');
+
 
 mongoose.connect(dbURI, { useMongoClient: true });
 
@@ -14,6 +18,13 @@ app.set('views', `${__dirname}/views`);
 app.use(expressLayouts);
 app.use(express.static(`${__dirname}/public`));
 app.use(morgan('dev'));
+app.use(methodOverride((req => {
+  if(req.body && typeof req.body === 'object' && '_method' in req.body) {
+    const method = req.body._method;
+    delete req.body._method;
+    return method;
+  }
+})));
 
 app.use(routes);
 
