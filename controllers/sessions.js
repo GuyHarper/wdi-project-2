df1ccadb2372b008b645127407f9d10e44a418dd
= <guy.harper@gmail.com>
2017-09-11 14:19:35 +0100

    Fixing login redirect in sessions controller

--- controllers/sessions.js
+++ controllers/sessions.js
@@ -10,7 +10,7 @@ function sessionsCreate(req, res) {
     .then(user => {
       if(!user || !user.validatePassword(req.body.password)) {
         req.flash('danger', 'Invalid credentials');
-        return res.reirect('/login');
+        return res.redirect('/login');
       }
       req.session.userId = user.id;
       res.redirect('/');
