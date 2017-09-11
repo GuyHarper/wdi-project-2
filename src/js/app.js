9c9adbd6462917c12c4150a988a3ae72e18ab480
= <guy.harper@gmail.com>
2017-09-11 15:42:58 +0100

    Adding comment button auto-hide functionality

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,21 @@
-console.log('Hello world');
+const $commentInput = $('.comment-input');
+let commentId = null;
+
+$commentInput.on('focus',() => {
+
+  const arrayOfClasses = $commentInput.attr('class').split(' ');
+  arrayOfClasses.forEach(function(element) {
+    if(element.match(/comment-id-*/)) {
+      commentId = element;
+    }
+  });
+  const $commentButton = $(`.comment-button.${commentId}`);
+  $commentButton.removeClass('hidden');
+});
+
+$commentInput.on('blur',() => {
+  if($commentInput.val() === '') {
+    const $commentButton = $(`.comment-button.${commentId}`);
+    $commentButton.addClass('hidden');
+  }
+});
